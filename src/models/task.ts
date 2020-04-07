6b074c331de1f28542b599b8920a532562c69658
azizmashkour <azizmashkour@gmail.com>
2020-04-07 15:19:59 +0100

    feat: create tasks model and add it private methods and constraints

---
+++ src/models/task.ts
@@ -0,0 +1,57 @@
+import { BaseModel } from './base-model'
+import { Member } from './member'
+
+export class Task extends BaseModel {
+  title: string
+  start: Date
+  due: Date
+  assignees: Member[] | undefined
+  description: string
+  comments: string
+  tags: string[]
+  status: 'New' | 'Pending' | 'In progress' | 'Completed' | 'Archieved' = 'New'
+
+  constructor (title: string, data: any) {
+    super()
+    this.title = title
+
+    this.start = data.start ? new Date(data.start) : new Date()
+    this.due = data.due ? new Date(data.due) : new Date()
+    this.description = data.description || ''
+    this.comments = data.comments || ''
+    this.tags = data.tags || []
+    this.status = data.status || 'New'
+
+    if (data.assignees) {
+      this.assignees = data.assignees.map(item => Member.build(item))
+    }
+  }
+
+  set checked (value: boolean) {
+    this.status = value ? 'Completed' : 'In progress'
+  }
+
+  get checked () {
+    return this.status === 'Completed'
+  }
+
+  clone () {
+    const obj = this.toJson()
+
+    return new Task(obj.title, obj)
+  }
+
+  toJson () {
+    return {
+      id: this.id,
+      title: this.title,
+      description: this.description,
+      comments: this.comments,
+      start: this.start.toString(),
+      due: this.due.toString(),
+      status: this.status,
+      tags: this.tags,
+      assignees: this.assignees?.map(member => member.toJson())
+    }
+  }
+}
